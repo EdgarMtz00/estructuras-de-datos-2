20c15a5e791908bbd85611d775ba0112daebf1d0
Edgar <edgar.agustin.martinez@gmail.com>
2021-08-29 12:18:00 -0500

    Complete file storage system

--- delimitadores/src/file_storage.rs
+++ delimitadores/src/file_storage.rs
@@ -12,39 +12,73 @@ pub struct FileStorage <T: fmt::Display> {
     file_output: File,
     buffered_reader: BufReader<File>,
     cursor: u64,
+    insert_pos: Vec<u64>,
 }
 
 impl <T: fmt::Display> FileStorage<T> {
     pub fn new(filename: String) -> Self {
-        let output = match OpenOptions::new().write(true).open(&filename){
+        let mut output = match OpenOptions::new().write(true).open(&filename){
             Ok(file) => file,
             Err(e) => panic!("Can't edit file due to: {}", e),
         };
 
-        let reader = match File::open(filename.to_string()){
+        let mut reader = match File::open(filename.to_string()){
             Ok(file) =>  BufReader::new(file),
             Err(e) => panic!("Can't open file due to: {}", e),
         };
 
+        let mut start_pos = 0;
+        let mut buf = String::new();
+        let mut cursor = 0;
+
+        reader.read_line(&mut buf);
+        if !buf.is_empty(){
+            let line = buf.chars().take_while(|&c| c != '\n').collect::<String>();
+            cursor = line.len() + 1;
+            let line = buf.chars().take_while(|&c| c != ' ').collect::<String>();
+            start_pos = line.parse::<u64>().unwrap();
+        }else{
+            write!(&mut output, "18               \n");
+            start_pos = 18;
+            cursor = 18;
+        }
+
         FileStorage::<T> {
             file_output: output,
             buffered_reader: reader,
-            cursor: 0,
-            _marker: PhantomData
+            cursor: cursor as u64,
+            _marker: PhantomData,
+            insert_pos: vec!(start_pos),
         }
     }
 
     pub fn write(&mut self, record: &T) -> Result<(), Error> {
-        &self.file_output.seek(SeekFrom::Start(self.cursor));
+        if self.insert_pos.len() == 1{
+            &self.file_output.seek(SeekFrom::Start(self.insert_pos[0]));
+            self.insert_pos[0] += record.to_string().len() as u64;
+        }else{
+            &self.file_output.seek(SeekFrom::Start(self.insert_pos.pop().unwrap()));
+        }
         write!(&self.file_output, "{}", record.to_string())?;
-        self.cursor += record.to_string().len() as u64;
+        Ok(())
+    }
+
+    fn write_char(&mut self, data: char) -> Result<(), Error> {
+        &self.file_output.seek(SeekFrom::Start(self.cursor));
+        write!(&self.file_output, "{}", data)?;
+        self.cursor += 1;
         Ok(())
     }
 
     pub fn read_line<'buf>(&mut self, buffer:&'buf mut String, restart: bool) -> Option<&'buf mut String>{
         buffer.clear();
-        self.cursor = if restart {0} else {self.cursor};
-        self.buffered_reader.seek(SeekFrom::Start(self.cursor));
+        if restart {
+            self.buffered_reader.seek(SeekFrom::Start(0));
+            self.buffered_reader.read_line(buffer);
+            buffer.clear();
+        }else{
+            self.buffered_reader.seek(SeekFrom::Start(self.cursor));
+        }
         let line = self.buffered_reader.read_line(buffer).map(|u| if u == 0 {None} else {Some(buffer)}).transpose();
         self.cursor = self.buffered_reader.seek(SeekFrom::Current(0)).unwrap();
         if line.is_none() {
@@ -78,27 +112,59 @@ impl <T: fmt::Display> FileStorage<T> {
         Some(attr)
     }
 
-    pub fn search(&mut self, first_element: impl PartialEq + fmt::Display) -> Option<T> where T: Deserializable{
-        let mut buffer = String::new();
+    fn search_line<'buf>(&mut self, id:impl PartialEq + fmt::Display, buffer:&'buf mut String) -> Option<&'buf mut String>{
         let mut reset = true;
-        while let Some(mut line) = self.read_line(&mut buffer, reset) {
+        //self.insert_pos = self.cursor;
+        while let Some(mut line) = self.read_line(buffer, reset) {
             reset = false;
             let line_start = line.len();
             let attribute = self.read_attribute(&mut line).unwrap();
-            if attribute == first_element.to_string() {
+            if attribute == id.to_string() {
                 self.cursor -= line_start as u64;
-                let mut line = self.read_line(& mut buffer, false).unwrap();
+                return self.read_line(buffer, false)
+            }
+        }
+        None
+    }
+
+    pub fn search(&mut self, id: impl PartialEq + fmt::Display) -> Option<T> where T: Deserializable{
+        let mut buffer = String::new();
+        match self.search_line(id, &mut buffer){
+            Some(mut line) => {
                 match self.deserialize(&mut line){
                     Ok(data) => {
                         return Some(data)
                     }
-                    Err(e) => {
+                    Err(_) => {
                         panic!("Data is corrupted");
                     }
                 }
-            }
+            },
+            None => None
         }
-        None
+    }
+
+    pub fn delete(&mut self, id: impl PartialEq + fmt::Display) -> Result<(), Error>{
+        let mut buffer = String::new();
+        match self.search_line(id, &mut buffer){
+            Some(line) => {
+                self.cursor = self.cursor - line.len() as u64;
+                self.insert_pos.push(self.cursor);
+                for _ in line.chars(){
+                    self.write_char(' ')?;
+                }
+                self.cursor -= 1;
+                self.write_char('\n')?;
+                Ok(())
+            },
+            None => Ok(())
+        }
+    }
+
+    pub fn modify(&mut self, id: impl PartialEq + fmt::Display, new_record: T) -> Result<(), Error>{
+        self.delete(id)?;
+        self.write(&new_record)?;
+        Ok(())
     }
 
     pub fn deserialize(&self, line: &mut String) -> Result<T, Error> where T: Deserializable{
@@ -107,6 +173,27 @@ impl <T: fmt::Display> FileStorage<T> {
             attributes.push(attr);
         }
         Ok(T::new(attributes))
+    }
+
+    pub fn save(&mut self){
+        let mut buffer = String::new();
+        self.cursor = 0;
+        let line = self.read_line(&mut buffer, false).unwrap();
+        let line = line.chars().take_while(|&c| c != ' ').collect::<String>();
+
+        let last_pos = self.insert_pos[0].to_string();
+
+        if line.len() > last_pos.len(){
+            self.cursor = 0;
+            for _ in 0..line.len(){
+                self.write_char(' ');
+            }
+        }
+
+        self.cursor = 0;
+        for digit in last_pos.chars() {
+            self.write_char(digit);
+        }
     }
 }
 
@@ -154,21 +241,29 @@ mod tests {
             assert_eq!(file.deserialize(line).unwrap(), records[i]);
             i += 1;
         }
-
+        file.save();
         Ok(())
     }
 
     #[test]
-    fn search() -> Result<(), Error>{
+    fn search_delete_and_search_again() -> Result<(), Error>{
         let filename = "test.txt";
         let mut file = FileStorage::<Record>::new(filename.to_string());
-        let records = vec!{Record{id: 1, data: String::from("data")}, Record{id: 2, data: String::from("data 2")}};
+        let records = vec!{Record{id: 1, data: String::from("not data")}, Record{id: 2, data: String::from("data 2")}};
 
         for record in &records {
             file.write(&record)?;
         }
-        assert_eq!(file.search(2).unwrap(), records[1]);
+        assert_eq!(file.search(1).unwrap(), records[0]);
 
+        file.delete(1)?;
+
+        assert_ne!(file.search(1).unwrap_or(Record{id:0, data:String::from("")}), records[0]);
+
+        file.write(&records[0])?;
+        file.modify(1, Record{id:1, data:String::from("new data")})?;
+        assert_eq!(file.search(1).unwrap(),Record{id:1, data:String::from("new data")});
+        file.save();
         Ok(())
     }
 }
